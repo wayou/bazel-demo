64f9dfad5e08b0fe846c25b969b83ae363c74665
wemlion <angusfu1126@qq.com>
2019-12-26 13:30:05 +0800

    WIP: fix runtime errors

--- index.tsx
+++ index.tsx
@@ -1,8 +1,8 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import * as styles from './styles.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from './styles.css';
 
 ReactDOM.render(
-  <h1 className={styles.h1}>Hello, world~</h1>,
+  <h1 className={styles.h1}>Hello, world!!</h1>,
   document.getElementById('root')
 );
